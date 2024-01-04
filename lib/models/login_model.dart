d8ca0617f6b76c7ee70bbd516fb7c7e46429004c
rishiakbari <akbaririshi6@gmail.com>
2024-01-04 12:01:10 +0530

    add a statusCode

--- lib/models/login_model.dart
+++ lib/models/login_model.dart
@@ -1,12 +1,13 @@
 
-
 class LoginModel {
     String? message;
     LoginBody? body;
+    int? statusCode;
 
     LoginModel({
         this.message,
         this.body,
+        this.statusCode,
     });
 
     factory LoginModel.fromJson(Map<String, dynamic> json) => LoginModel(
@@ -21,113 +22,25 @@ class LoginModel {
 }
 
 class LoginBody {
+    int? id;
+    String? username;
     String? token;
-    User? user;
 
     LoginBody({
+        this.id,
+        this.username,
         this.token,
-        this.user,
     });
 
     factory LoginBody.fromJson(Map<String, dynamic> json) => LoginBody(
-        token: json["token"],
-        user: json["user"] == null ? null : User.fromJson(json["user"]),
-    );
-
-    Map<String, dynamic> toJson() => {
-        "token": token,
-        "user": user?.toJson(),
-    };
-}
-
-class User {
-    int? id;
-    dynamic email;
-    dynamic name;
-    String? avatar;
-    String? countryCode;
-    String? phone;
-    String? referralCode;
-    dynamic address;
-    dynamic city;
-    dynamic state;
-    dynamic country;
-    dynamic zipCode;
-    dynamic gst;
-    String? wallet;
-    String? cardLimit;
-    dynamic expiryDate;
-    String? paid;
-    dynamic paymentId;
-    String? referralText;
-    int? referralCount;
-
-    User({
-        this.id,
-        this.email,
-        this.name,
-        this.avatar,
-        this.countryCode,
-        this.phone,
-        this.referralCode,
-        this.address,
-        this.city,
-        this.state,
-        this.country,
-        this.zipCode,
-        this.gst,
-        this.wallet,
-        this.cardLimit,
-        this.expiryDate,
-        this.paid,
-        this.paymentId,
-        this.referralText,
-        this.referralCount,
-    });
-
-    factory User.fromJson(Map<String, dynamic> json) => User(
         id: json["id"],
-        email: json["email"],
-        name: json["name"],
-        avatar: json["avatar"],
-        countryCode: json["country_code"],
-        phone: json["phone"],
-        referralCode: json["referral_code"],
-        address: json["address"],
-        city: json["city"],
-        state: json["state"],
-        country: json["country"],
-        zipCode: json["zip_code"],
-        gst: json["gst"],
-        wallet: json["wallet"],
-        cardLimit: json["card_limit"],
-        expiryDate: json["expiry_date"],
-        paid: json["paid"],
-        paymentId: json["payment_id"],
-        referralText: json["referral_text"],
-        referralCount: json["referral_count"],
+        username: json["username"],
+        token: json["token"],
     );
 
     Map<String, dynamic> toJson() => {
         "id": id,
-        "email": email,
-        "name": name,
-        "avatar": avatar,
-        "country_code": countryCode,
-        "phone": phone,
-        "referral_code": referralCode,
-        "address": address,
-        "city": city,
-        "state": state,
-        "country": country,
-        "zip_code": zipCode,
-        "gst": gst,
-        "wallet": wallet,
-        "card_limit": cardLimit,
-        "expiry_date": expiryDate,
-        "paid": paid,
-        "payment_id": paymentId,
-        "referral_text": referralText,
-        "referral_count": referralCount,
+        "username": username,
+        "token": token,
     };
 }
