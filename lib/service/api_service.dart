4f2b6aca055d05164bdb835443a68bb7797f159f
rishiakbari <akbaririshi6@gmail.com>
2024-01-20 19:46:15 +0530

    Create a API Services of profile and Update profile

--- lib/service/api_service.dart
+++ lib/service/api_service.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:taptrend/models/login_model.dart';
+import '../models/profile_model.dart';
 import '../utils/api.dart';
 import '../utils/helper.dart';
 
@@ -35,6 +36,77 @@ class ApiService {
       return LoginModel.fromJson(jsonDecode(response.body));
     } catch (e) {
       return LoginModel.fromJson(
+          jsonDecode('{"message":"Some technical issue! Please try again"}'));
+    }
+  }
+
+  static Future<ProfileModel> profile({
+    
+    required String token,
+    // required String password,
+
+  
+  }) async {
+    
+
+    try {
+           _headers.addAll({'Authorization': 'Bearer $token'});
+      final response = await _client.get(
+        Api.authProfile,
+        headers: _headers,
+      );
+     
+
+      if (response.statusCode == 401) {
+        Helper.logout();
+        return ProfileModel.fromJson(jsonDecode(''));
+      }
+
+      // print(response.body);
+      return ProfileModel.fromJson(jsonDecode(response.body));
+    } catch (e) {
+      return ProfileModel.fromJson(
+          jsonDecode('{"message":"Some technical issue! Please try again"}'));
+    }
+  }
+
+  // Update Profile
+  static Future<ProfileModel> updateProfile({
+    
+    required String token,
+    required String name,
+    required String phone,
+    required String email,
+
+    // required String avatar,
+  }) async {
+    
+
+    try {
+           _headers.addAll({'Authorization': 'Bearer $token'});
+           http.MultipartRequest request =
+           http.MultipartRequest('POST', Api.authProfile);
+           request.headers.addAll(_headers);
+           request.fields.addAll({
+           '_method': 'PATCH',
+           'name': name,
+           'country_code': '91',
+           'phone': phone,
+           'email': email,
+        // 'distributor_code': distributorCode,
+      });
+      http.Response response =
+          await http.Response.fromStream(await request.send());
+      if (response.statusCode == 401) {
+        Helper.logout();
+        return ProfileModel.fromJson(jsonDecode(''));
+      }
+      ProfileModel profile = ProfileModel.fromJson(jsonDecode(response.body));
+      profile.statusCode = response.statusCode;
+      return profile;
+   
+    } catch (e) {
+      return ProfileModel.fromJson(
           jsonDecode('{"message":"Some technical issue! Please try again"}'));
     }
   }
